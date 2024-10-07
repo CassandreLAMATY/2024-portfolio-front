894e4a062a55ec4af75c9e6dfe9e33815201f2c6
CassandreLAMATY <cassandre.lamaty@gmail.com>
2024-10-07 14:36:07 +0200

    non opti responsive init (rework needed)

--- src/blocks/HeaderBlock.vue
+++ src/blocks/HeaderBlock.vue
@@ -18,26 +18,29 @@
             </div>
 
             <div class="mountains">
-                <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" class="mountain" id="mountain01">
-                    <polygon points="100 -50, 100 100, 0 100"/>
-                </svg>
-                <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" class="mountain" id="mountain02">
-                    <polygon points="50 15, 120 100, 0 100"/>
-                </svg>
-                <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" class="mountain" id="mountain03">
-                    <polygon points="50 50, 100 100, 15 100"/>
-                </svg>
-                <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" class="mountain" id="mountain04">
-                    <polygon points="0 15, 100 100, 0 100"/>
-                </svg>
+                <div class="mountains-container">
+                    <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" class="mountain" id="mountain01">
+                        <polygon points="100 -50, 100 100, 0 100"/>
+                    </svg>
+                    <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" class="mountain" id="mountain02">
+                        <polygon points="50 15, 120 100, 0 100"/>
+                    </svg>
+                    <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" class="mountain" id="mountain03">
+                        <polygon points="50 50, 100 100, 15 100"/>
+                    </svg>
+                    <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" class="mountain" id="mountain04">
+                        <polygon points="0 15, 100 100, 0 100"/>
+                    </svg>
+                </div>
+                <div id="mountain01-body"></div>
             </div>
         </div>
-        <div class="me-container">
+        <!-- <div class="me-container">
             <div class="me">
                 <h3 class="me-overline">Hello, world ! I'm</h3>
                 <h1 class="me-name"><span class="me-name" id="firstname">Cassandre</span> <span class="me-name" id="name">LAMATY</span></h1>
             </div>
-        </div>
+        </div> -->
     </header>
 </template>
 
