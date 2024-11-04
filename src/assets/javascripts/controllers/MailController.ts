11874fed619eda83ff6b4c79e7983f6b1f7c9aaf
CassandreLAMATY <cassandre.lamaty@gmail.com>
2024-11-04 10:56:31 +0100

    init controller mail

---
+++ src/assets/javascripts/controllers/MailController.ts
@@ -0,0 +1,7 @@
+export class MailController {
+    public name: string = '';
+    public email: string = '';
+    public object: 'info' | 'work' | 'other' = 'info';
+    public customObject: string = '';
+    public message: string = '';
+}
