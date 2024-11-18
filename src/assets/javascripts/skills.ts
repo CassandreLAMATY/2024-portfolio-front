18d8c1c4c14149a195477e41304cc4a16348b240
CassandreLAMATY <cassandre.lamaty@gmail.com>
2024-11-18 13:54:20 +0100

    install axios & dotenv

--- src/assets/javascripts/skills.ts
+++ src/assets/javascripts/skills.ts
@@ -1,5 +1,9 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import { apiHandler } from '@/main';
+const apiToken = process.env.VUE_APP_API_TOKEN;
 
 export async function getSkillTags() {
-    const tags = await apiHandler.get('/skills/tags');
+    const tags = await apiHandler.get('/skills/tags', {});
 }
