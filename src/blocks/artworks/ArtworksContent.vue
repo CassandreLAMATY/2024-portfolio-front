8c37ce0b0ca4e5cc480da91f2db1e35132982052
CassandreLAMATY <cassandre.lamaty@gmail.com>
2024-12-09 16:00:41 +0100

    feat(artworks content): content

--- src/blocks/artworks/ArtworksContent.vue
+++ src/blocks/artworks/ArtworksContent.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { isMobile, isTablet } from '@/assets/javascripts/global';
 import type { Artwork } from '@/assets/javascripts/types/Artwork';
 
 defineProps<{ artworks: Artwork[] }>();
@@ -10,7 +11,19 @@ const apiUrl = import.meta.env.VITE_STRAPI_URL;
     <section class="artworks">
         <div class="artworks-container">
             <div v-for="(a, i) of artworks" :key="i" class="artwork">
-                <div v-if="a.render && a.render.formats && a.render.formats.small" class="artwork-img--container">
+                <a v-if="a.links" :href="a.links.link" target="_blank">
+                    <div
+                        v-if="a.render && a.render.formats && a.render.formats.small && a.render.formats.medium"
+                        class="artwork-img--container"
+                    >
+                        <img
+                            :src="isTablet ? apiUrl + a.render.formats.small.url : apiUrl + a.render.formats.medium.url"
+                            :alt="a.description"
+                            class="artwork-img"
+                        />
+                    </div>
+                </a>
+                <div v-else-if="a.render && a.render.formats && a.render.formats.small" class="artwork-img--container">
                     <img :src="apiUrl + a.render.formats.small.url" :alt="a.description" class="artwork-img" />
                 </div>
             </div>
